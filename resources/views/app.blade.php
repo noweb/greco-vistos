41d6e038adcdf975a64cb85ac3e21862940e1566
Victor da Silva Fernandes <60992300+VictordaSilvaf@users.noreply.github.com>
2025-06-06 16:23:10 -0300

    Add Google Tag Manager integration to app.blade.php
    - Included Google Tag Manager script for tracking and analytics.
    - Added noscript fallback for users with JavaScript disabled.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -47,10 +47,34 @@
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-TDSFSVT8');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="font-sans antialiased">
     @inertia
+
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDSFSVT8" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
 </body>
 
 </html>
